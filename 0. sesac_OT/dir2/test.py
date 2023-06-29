858ff990a03e8e4fbfd788257a655ed81fe61a88
KIM SOOBIN <xpsxm225@naver.com>
2023-06-29 12:12:16 +0900

    csv파일 clean버전

--- 0. sesac_OT/dir2/test.py
+++ 0. sesac_OT/dir2/test.py
@@ -16,8 +16,10 @@ import csv
 #     for row in csv_reader:
 #         users.append(row)
 # print(users)
+user_info_dic_list = []
 with open('user.csv', 'r', encoding="utf-8") as file:
         csv_data = csv.DictReader(file)
         for data in csv_data:
-            print(data) 
-        
+                user_info_dic_list.append(data) 
+
+print(user_info_dic_list)
--- 4. Flask/data_generator/data_generator.py
+++ 4. Flask/data_generator/data_generator.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     user_list = []
-    with open("user.csv", "r", encoding="utf-8") as file:
-        csv_reader = csv.reader(file)
-
-        for row in csv_reader:
-            user_list.append(row)
-
-    return render_template("main.html", user_info = user_list)
+    with open('user.csv', 'r', encoding="utf-8") as file:
+        csv_data = csv.DictReader(file)
+        for data in csv_data:
+                user_list.append(data) 
+    print(user_list)
+    return render_template("main.html", data= user_list)
 
 @app.route("/user/detail/<user_id>")
 def user_detail(user_id):
@@ -23,14 +22,25 @@ def user_detail(user_id):
         for row in reader:
             if user_id in row :
                 user_info = row
-    # 딕셔너리 형태로 csv파일 담기
-    # with open('user.csv', 'r', encoding="utf-8") as file:
-    #         csv_data = csv.DictReader(file)
-    #         for data in csv_data:
-    #             print(data) 
-    
     return render_template("user/detail.html", user_info = user_info)
 
+@app.route("/index")
+def index():
+    # 한 페이지에 담길 컨텐츠 수를 정한다.(num)
+    per_page = 10
+    total_pages = None
+    start_index = None
+    
+    data = []
+    with open('data_space.csv', 'r', encoding="utf-8") as file:
+        csv_data = csv.DictReader(file)
+        headers = [header.strip() for header in csv_data.fieldnames]
+        
+        for row in csv_data:
+            clean_row = {key.strip() : value.strip() for key, value in row.items()}
+            data.append(clean_row) 
+        
+    return render_template("main.html", data = data)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
