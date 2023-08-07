77cdbcf5b295c0fd81bc4207c4ba437544c0eb0b
KIM SOOBIN <xpsxm225@naver.com>
2023-08-07 12:33:43 +0900

    뉴스 본문 가져오기

--- 11. crawling/naver_news.py
+++ 11. crawling/naver_news.py
@@ -11,8 +11,6 @@ def get_requests_html_parser(url) :
 
     return soup
 
-# print(soup)
-
 def get_news_info() :
     soup = get_requests_html_parser(naver_sports)
     news_text = soup.select('.today_list > li')
@@ -26,10 +24,23 @@ def get_news_info() :
         news_info.append(news)
         print(news)
         
-def get_land_info() :
+def get_land_info(type) :
+    types = ['headline', 'headline_content', 'report']
+    if type not in types :  
+        return print("Unknow Type")
+
     soup = get_requests_html_parser(naver_land)
-    get_headline_text(soup)
-    get_report_text(soup)
+    if type == 'headline' :
+        return get_headline_text(soup)
+    
+    if type == 'headline_content' :
+        soup = get_requests_html_parser(naver_land)
+        return get_headline_content(soup)
+    
+    if type == 'report' :
+        soup = get_requests_html_parser(naver_land)
+        return get_report_text(soup)
+
 
 def get_headline_text(soup) :
     land_text = soup.select('.section_group:first-child > ul.list_type > li a:nth-child(2)')
@@ -43,6 +54,25 @@ def get_report_text(soup) :
         for report_content in a:
             print(f'report_content : {report_content}')
 
+def get_headline_href(soup) :
+    land_text = soup.select('.section_group:first-child > ul.list_type > li a:nth-child(2)')
+    content_url_list = ['https://land.naver.com' + l.attrs['href'] for l in land_text]
+
+    return content_url_list
+
+def get_headline_content(soup) :
+    content_url_list = get_headline_href(soup)
+
+    for url in content_url_list :
+        soup = get_requests_html_parser(url)
+        content = soup.select_one('#articleBody').text
+        print(f'content : content')
+
+# 미션 3. 저 헤드라인 기사의 본문을 가져오기
+
 if __name__ == '__main__' :
     # get_news_info()
-    get_land_info()
+    # get_land_info('headline')
+    # get_land_info('report')
+    # get_land_info('reporadsft')
+    get_land_info('headline_content')
