01e52a1754bab1250ddfab121acfcb193eacd59f
KIM SOOBIN <xpsxm225@naver.com>
2023-08-09 10:48:07 +0900

    gitignore update

--- 12. API/papago.py
+++ 12. API/papago.py
@@ -1,8 +1,11 @@
 import os
 import sys
 import urllib.request
-client_id = "YOUR_CLIENT_ID" # 개발자센터에서 발급받은 Client ID 값
-client_secret = "YOUR_CLIENT_SECRET" # 개발자센터에서 발급받은 Client Secret 값
+import json
+
+client_id = "gkGXvGKvhCeCUBJGi786" # 개발자센터에서 발급받은 Client ID 값
+client_secret = open("secret.txt", "r").read() # 개발자센터에서 발급받은 Client Secret 값
+
 encText = urllib.parse.quote("반갑습니다")
 data = "source=ko&target=en&text=" + encText
 url = "https://openapi.naver.com/v1/papago/n2mt"
@@ -11,8 +14,11 @@ request.add_header("X-Naver-Client-Id",client_id)
 request.add_header("X-Naver-Client-Secret",client_secret)
 response = urllib.request.urlopen(request, data=data.encode("utf-8"))
 rescode = response.getcode()
+
 if(rescode==200):
     response_body = response.read()
-    print(response_body.decode('utf-8'))
+    # print(response_body.decode('utf-8'))
+    data = json.loads(response_body)
+    print(data['message']['result']['translatedText'])
 else:
     print("Error Code:" + rescode)
